d624df5d95cef51fe6471e66fb9ed93ee25e9b7a
d3mi4an <cuentaparafrifrayer255@gmail.com>
2026-09-06 02:27:44 -0300

    Create Server.js

---
+++ Server.js
@@ -0,0 +1,25 @@
+const express = require("express");
+const http = require("http");
+const WebSocket = require("ws");
+
+const app = express();
+const server = http.createServer(app);
+const wss = new WebSocket.Server({ server });
+
+app.use(express.static("."));
+
+wss.on("connection", (ws) => {
+  ws.on("message", (message) => {
+    wss.clients.forEach((client) => {
+      if (client !== ws && client.readyState === WebSocket.OPEN) {
+        client.send(message.toString());
+      }
+    });
+  });
+});
+
+const PORT = process.env.PORT || 3000;
+
+server.listen(PORT, () => {
+  console.log(`Servidor funcionando en el puerto ${PORT}`);
+});
